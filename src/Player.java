4d316c46e0e72be00117282bce2e20c7eb5300fe
d.azarenko <azarenko1993@gmail.com>
2023-10-18 16:48:41 +0300

    commit for merge

--- src/Player.java
+++ src/Player.java
@@ -2,20 +2,6 @@ import ui.Screen;
 
 class Player {
     private Song nameOfSong;
-    private Song nameOfSong2;
-    private Song nameOfSong3;
-    private Song nameOfSong4;
-    private Song nameOfSong5;
-    private Song nameOfSong6;
-    private Song nameOfSong7;
-    private Song nameOfSong8;
-    private Song nameOfSong13;
-    private Song nameOfSong12;
-    private Song nameOfSong11;
-    private Song nameOfSong14;
-    private Song nameOfSong15;
-    private Song nameOfSong113;
-    private Song nameOfSong111;
 
     public Player(Song nameOfSong) {
         this.nameOfSong = nameOfSong;

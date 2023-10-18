0d6ff5bb8f954e2326fcb36ad74f00cde14bb3a8
d.azarenko <azarenko1993@gmail.com>
2023-10-18 13:57:06 +0300

    devTest3

--- src/Player.java
+++ src/Player.java
@@ -4,6 +4,12 @@ class Player {
     private Song nameOfSong;
     private Song nameOfSong2;
     private Song nameOfSong3;
+    private Song nameOfSong4;
+    private Song nameOfSong5;
+    private Song nameOfSong6;
+    private Song nameOfSong7;
+    private Song nameOfSong8;
+    private Song nameOfSong9;
 
     public Player(Song nameOfSong) {
         this.nameOfSong = nameOfSong;

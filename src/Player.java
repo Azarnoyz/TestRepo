ae66ba9fa902aa430af23b9f1643bfdb3eb4b5cf
d.azarenko <azarenko1993@gmail.com>
2023-10-18 16:44:29 +0300

    commit for chery pick2

--- src/Player.java
+++ src/Player.java
@@ -14,6 +14,7 @@ class Player {
     private Song nameOfSong11;
     private Song nameOfSong14;
     private Song nameOfSong15;
+    private Song nameOfSong111;
 
     public Player(Song nameOfSong) {
         this.nameOfSong = nameOfSong;

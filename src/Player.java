8a926cc027ad13262c0508aa5f45ad635cd655ab
d.azarenko <azarenko1993@gmail.com>
2023-10-19 21:08:48 +0300

    remove two new songs

--- src/Player.java
+++ src/Player.java
@@ -2,8 +2,6 @@ import ui.Screen;
 
 class Player {
     private Song nameOfSong;
-    private Song nameOfSong2;
-    private Song nameOfSong3;
 
     public Player(Song nameOfSong) {
         this.nameOfSong = nameOfSong;

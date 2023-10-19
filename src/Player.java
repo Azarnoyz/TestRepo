413653ec2871fa35af67e819e7d2f4ab2ef33958
d.azarenko <azarenko1993@gmail.com>
2023-10-19 21:16:27 +0300

    add 34

--- src/Player.java
+++ src/Player.java
@@ -4,6 +4,7 @@ class Player {
     private Song nameOfSong;
     private Song nameOfSong2;
     private Song nameOfSong3;
+    private Song nameOfSong34;
 
     public Player(Song nameOfSong) {
         this.nameOfSong = nameOfSong;
